281143cd5e568033c1b3b6d11b9eec736e9f7676
root <0pq76r@gmail.com>
2012-06-23 11:22:10 +0200

    folder tree

---
+++ init.h
@@ -0,0 +1,12 @@
+
+/************************************************
+ * 
+ * Copyright (c) 2012 Patrick Eigensatz
+ * Copyright (c) 2012 Philipp Muggli
+ * Copyright (c) 2012 Florian Wernli
+ * 
+ *
+ * This file is released under the GPLv3.
+ *
+ ***********************************************/
+
