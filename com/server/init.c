9a543346f206484e5ae6ac7a95b9000b07e9fd69
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2012-06-24 14:42:31 +0200

    Updated header as ~ recommended by the FSF

--- com/server/init.c
+++ com/server/init.c
@@ -1,12 +1,14 @@
+/*
+ * Copyright 2012 the "Cubebox Team"
+ * See http://cubebox.bplaced.net/about.php for a full list.
+ * 
+ * This file is licensed under the GNU General Public License v3.
+ * You find the license here:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ * This software is distributed AS-IS and WITHOUT ANY WARRANTY.
+ * See the license file for details
+ * 
+*/
 
-/************************************************
- * 
- * Copyright (c) 2012 Patrick Eigensatz
- * Copyright (c) 2012 Philipp Muggli
- * Copyright (c) 2012 Florian Wernli
- * 
- *
- * This file is released under the GPLv3.
- *
- ***********************************************/
 
