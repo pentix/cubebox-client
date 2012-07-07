227f18f7a73caffc1223c1076ece637bb7c62965
root <0pq76r@gmail.com>
2012-07-07 16:41:07 +0200

    Added map/

---
+++ map/main.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2012, The Cubebox Team <http://cubebox.bplaced.net/>
+ * 
+ * 
+ * This file is part of cubebox.
+ * 
+ * cubebox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * cubebox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with cubebox.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+
+#ifndef __MAP_MAIN_H__
+#define __MAP_MAIN_H__
+
+extern void map();
+
+#endif
